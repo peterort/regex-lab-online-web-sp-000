2a7e066b5eff27fb7ed9f746cdd99060aa6bec2d
Peter Ort <peter@peterort.com>
2020-04-02 21:22:48 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -4,11 +4,10 @@ end
 
 def words_starting_with_un_and_ending_with_ing(text)
   text.scan(/un\w*ing/)
-
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b[a-z]{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
